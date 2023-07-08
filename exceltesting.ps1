05192083db3c36f890fe3de0a9eda78696a16b55
James Collins <41191146+Quacky74@users.noreply.github.com>
2023-07-08 11:33:29 +1000

    Initial comment
    First comment of the project

---
+++ exceltesting.ps1
@@ -0,0 +1,39 @@
+﻿#the purpose of this scrpit is to act as a testing groud for working with excel
+#Author James Collins
+
+
+function MakeNewWorksheet($sheetName)
+{
+
+}
+
+#ref excel object
+$ExcelObject = New-Object -ComObject Excel.Application
+
+#make the excel window visable
+$ExcelObject.Visible=$true
+
+#Open workbook
+$ActiveWorkbook = $ExcelObject.workbooks.Open("D:\Projects\powershell\file1.xlsx")
+
+#set active worksheet
+$ActiveWorkSheet = $ActiveWorkbook.Sheets.Item("D1")
+
+$d1UsedRange = $ActiveWorkSheet.UsedRange
+$d1Data = $d1UsedRange.Value2
+
+
+
+echo "Summary of Excel Sheet"
+
+foreach($row in $d1Data)
+{
+    foreach($cell in $row)
+    {
+        Write-Host $cell
+    }
+
+}
+
+$ActiveWorkbook.Close($true)
+$ExcelObject.Quit()
